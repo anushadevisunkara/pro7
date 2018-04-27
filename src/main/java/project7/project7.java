ef81cf1acf9644780c993948e642c235fd36c704
anusha <anushadevi.sunkara@gmail.com>
2018-04-27 09:05:19 +0530

    initial commit

---
+++ src/main/java/project7/project7.java
@@ -0,0 +1,9 @@
+package project7;
+
+public class project7 {
+	public String sample() {
+		return "this is deveops progam";
+		
+	}
+
+}
---
+++ src/test/java/project7/pro7.java
@@ -0,0 +1,13 @@
+package project7;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class pro7 {
+
+	@Test
+	public void test() {
+		}
+
+}
